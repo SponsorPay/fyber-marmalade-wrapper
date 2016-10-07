455646e1a34490e41e3ac605af2c67469696acbc
Emory Petermann <emory@heyzap.com>
2016-10-07 14:16:27 -0700

    clean up java code and make compatible with latest marmalade

--- ExtFyber/source/android/FyberActivity.java
+++ ExtFyber/source/android/FyberActivity.java
@@ -14,14 +14,15 @@ import com.fyber.requesters.VirtualCurrencyCallback;
 import com.fyber.requesters.VirtualCurrencyRequester;
 import com.fyber.utils.FyberLogger;
 
-import com.ideaworks3d.marmalade.LoaderAPI;
+import com.ideaworks3d.marmalade.LoaderActivity;
 
 import android.content.Intent;
 import android.util.Log;
 import android.os.Bundle;
+import android.app.Activity;
 
 @FyberSDK
-public class FyberActivity implements RequestCallback {
+public class FyberActivity extends LoaderActivity implements RequestCallback {
 	protected static final int INTERSTITIAL_REQUEST_CODE = 8792;
 	protected static final int OFFERWALL_REQUEST_CODE = 8795;
 	protected static final int REWARDED_VIDEO_REQUEST_CODE = 8796;
@@ -35,7 +36,7 @@ public class FyberActivity implements RequestCallback {
 	private static final int EXT_FYBER_AD_NOT_AVAILABLE = 102;
 	private static final int EXT_FYBER_AD_REQUEST_ERROR = 103;
 
-	private static final String TAG = "marmalade";
+	private static final String TAG = "FyberActivity";
 	private static final String DEBUG_CONFIG_KEY = "ExtFyber_debug";
   
 	private static boolean DEBUG_ENABLED;
@@ -55,7 +56,7 @@ public class FyberActivity implements RequestCallback {
 
 
 	void debugLog(String message) {
-        Log.d(TAG, "FyberActivity:" + message);
+        Log.d(TAG, message);
     }
 
 	public void setup(String appId, String securityToken, String userId, String bucketId, String conditionGroupId) {
@@ -63,28 +64,10 @@ public class FyberActivity implements RequestCallback {
 		try {
 			debugLog("setup appId = " + appId + "; securityToken = " + securityToken + "; userId = " + userId + "; bucketId = " + bucketId + "; conditionGroupId = " + conditionGroupId);
 
-// 			Fyber.Settings fyberSettings = Fyber
-// 					.with(APP_ID, LoaderAPI.getActivity())
-// 					.withSecurityToken(SECURITY_TOKEN)
-// // by default Fyber SDK will start precaching. If you wish to only start precaching at a later time you can uncomment this line and use 'CacheManager' to start, pause or resume on demand.
-// //					.withManualPrecaching()
-// // if you do not provide an user id Fyber SDK will generate one for you
-// //					.withUserId(USER_ID)
-// 					.start();
-			// ** SDK INITIALIZATION **
-
-			//when you start Fyber SDK you get a Settings object that you can use to customise the SDK behaviour.
-			//Have a look at the method 'customiseFyberSettings' to learn more about possible customisation.
 			Fyber.Settings fyberSettings = Fyber
-					.with(appId, LoaderAPI.getActivity())
+					.with(appId, this)
 					.withSecurityToken(securityToken)
-// by default Fyber SDK will start precaching. If you wish to only start precaching at a later time you can uncomment this line and use 'CacheManager' to start, pause or resume on demand.
-//					.withManualPrecaching()
-// if you do not provide an user id Fyber SDK will generate one for you
-					.withUserId(userId)
 					.start();
-// uncomment to customise Fyber SDK
-//			customiseFyberSettings(fyberSettings);
 
 			User.addCustomValue("bucketId", bucketId);
 			User.addCustomValue("conditionGroupId", conditionGroupId);
@@ -94,23 +77,8 @@ public class FyberActivity implements RequestCallback {
 		} catch (IllegalArgumentException e) {
 			debugLog(e.getLocalizedMessage());
 		}
+		ExtFyber.safe_notifyStatusChange(EXT_FYBER_STARTED);
 	}
-
-	private void customiseFyberSettings(Fyber.Settings fyberSettings) {
-		fyberSettings.notifyUserOnReward(false)
-				.closeOfferWallOnRedirect(true)
-				.notifyUserOnCompletion(true)
-				.addParameter("myCustomParamKey", "myCustomParamValue")
-				.setCustomUIString(Fyber.Settings.UIStringIdentifier.GENERIC_ERROR, "my custom generic error msg");
-	}
-
-    public void onDestroy() {
-    	singleton = null;
-    }
-
-    public void finish() {
-        onDestroy();
-    }
 
     // when a button is clicked, request or show the ad according to Intent availability
 	public boolean showAd() {
@@ -119,7 +87,7 @@ public class FyberActivity implements RequestCallback {
 			//if we already have an Intent, we start the ad Activity
 			if (isIntentAvailable()) {
 				//start the ad format specific Activity
-				LoaderAPI.getActivity().startActivityForResult(intent, getRequestCode());
+				this.startActivityForResult(intent, getRequestCode());
 				return true;
 			} else {
 				isRequestingState = true;
@@ -132,18 +100,12 @@ public class FyberActivity implements RequestCallback {
 	}
 
 	public void performRequest() {
-		if (isIntentAvailable()) {
-			return;
-		}
-		//Requesting a rewarded video ad
 		RewardedVideoRequester
-				.create(this)
-						// you can add a virtual Currency Requester by chaining this extra method
-				// .withVirtualCurrencyRequester(getVirtualCurrencyRequester())
-				.request(LoaderAPI.getActivity());
+            .create(this)
+            .request(this);
 	}
 
-		@Override
+    @Override
 	public void onAdAvailable(Intent intent) {
 		resetRequestingState();
 		this.intent = intent;
@@ -152,7 +114,6 @@ public class FyberActivity implements RequestCallback {
 
 	@Override
 	public void onAdNotAvailable(AdFormat adFormat) {
-		debugLog("No ad available");
 		resetRequestingState();
 		resetIntent();
 		ExtFyber.safe_notifyStatusChange(EXT_FYBER_AD_NOT_AVAILABLE);
@@ -160,7 +121,7 @@ public class FyberActivity implements RequestCallback {
 
 	@Override
 	public void onRequestError(RequestError requestError) {
-		debugLog("Semething went wrong with the request: " + requestError.getDescription() + ";" + requestError);
+		debugLog(requestError.getDescription());
 		resetRequestingState();
 		resetIntent();
 		ExtFyber.safe_notifyStatusChange(EXT_FYBER_AD_REQUEST_ERROR);
@@ -182,23 +143,28 @@ public class FyberActivity implements RequestCallback {
 		return isRequestingState;
 	}
 
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		debugLog("requestCode = " + requestCode + "; resultCode = " + resultCode + "; data = " + data);
+    @Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		resetIntent();
 
-		if (data != null) {
-			debugLog("ENGAGEMENT_STATUS = " + data.getStringExtra("ENGAGEMENT_STATUS"));
+		if (resultCode == Activity.RESULT_OK && requestCode == REWARDED_VIDEO_REQUEST_CODE) {
 
-			String status = data.getStringExtra("ENGAGEMENT_STATUS");
-			if ("CLOSE_ABORTED".equals(status)) {
-				ExtFyber.safe_notifyStatusChange(EXT_FYBER_CLOSE_ABORTED);
-			} else if ("CLOSE_FINISHED".equals(status)) {
-				ExtFyber.safe_notifyStatusChange(EXT_FYBER_CLOSE_FINISHED);
-			} else if ("ERROR".equals(status)) {
-				ExtFyber.safe_notifyStatusChange(EXT_FYBER_ERROR);
-			} else {
-				debugLog("unknown status: " + status);
-			}
+			String status = data.getStringExtra(RewardedVideoActivity.ENGAGEMENT_STATUS);
+			debugLog("ENGAGEMENT_STATUS = " + status);
+
+            switch(status) {
+                case RewardedVideoActivity.REQUEST_STATUS_PARAMETER_ABORTED_VALUE:
+                    ExtFyber.safe_notifyStatusChange(EXT_FYBER_CLOSE_ABORTED);
+                    break;
+                case RewardedVideoActivity.REQUEST_STATUS_PARAMETER_FINISHED_VALUE:
+                    ExtFyber.safe_notifyStatusChange(EXT_FYBER_CLOSE_FINISHED);
+                    break;
+                case RewardedVideoActivity.REQUEST_STATUS_PARAMETER_ERROR:
+                    ExtFyber.safe_notifyStatusChange(EXT_FYBER_ERROR);
+                    break;
+                default:
+                    debugLog("Unknown Engagement Status: " + status);
+            }
 		}	
 	}
 
